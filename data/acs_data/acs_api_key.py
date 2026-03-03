a2205ea50bb94c11430b0359f0e1a2c721ea8588
Sam Roffman <sroffman3@gatech.edu>
2026-03-03 13:45:56 -0500

    Re-add data and init main

---
+++ data/acs_data/acs_api_key.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This loads the .env file
+
+ACS_API_KEY = os.getenv("ACS_API_KEY")
+
+print(ACS_API_KEY) 
---
+++ main.py
@@ -0,0 +1,3 @@
+from data.acs_data.acs_api_key import ACS_API_KEY
+
+print(ACS_API_KEY)
